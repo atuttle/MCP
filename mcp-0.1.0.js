fb34c0d93714977c9221ad083a51bddbd2fb5ae2
Adam Tuttle <adam@fusiongrokker.com>
2012-12-23 11:34:15 -0500

    WIP on .onPage

--- mcp-0.1.0.js
+++ mcp-0.1.0.js
@@ -16,6 +16,33 @@ var MCP = function(options){
 	this.options.loadFromCache = this.options.loadFromCache || [];
 	this.options.ready = this.options.ready || function(){/* noop */};
 
+	//to allow deeply nested stuff access to the root `this` object
+	var _mcp = this;
+
+	/*
+		PRIVATE METHODS
+	*/
+	var _renderTemplates = {};
+	function render(template, container, data, replace, append){
+		//defaults
+		replace = replace || true;
+		modifier = modifier || true;
+		//compile and cache template if it's not already cached
+		if (!_renderTemplates[template]){
+			_renderTemplates[template] = Handlebars.compile($( template ).html());
+		}
+		//render
+		var src = _renderTemplates[template]( data );
+		//clear existing contents if replace == true
+		if (replace) $( container ).empty();
+		//append or prepend as necessary (default is append; default for replace is true,
+		//so default is functionally equivalent to .html())
+		if (append)
+			$( src ).appendTo( container );
+		else
+			$( src ).prependTo( container );
+	}
+
 	/*
 		QUEUES
 	*/
@@ -115,6 +142,7 @@ var MCP = function(options){
 		return newQueue;
 	};
 	
+	var _pageDataCache = {};
 	this.onPage = function(options){
 		if (typeof options === 'undefined') throw "options argument is required";
 		if (typeof options.page === 'undefined') throw "page key of options argument is required";
@@ -123,7 +151,55 @@ var MCP = function(options){
 		if (typeof options.container === 'undefined') throw "container key of options argument is required";
 
 		$(options.page).on("pageshow", function(){
+			var requestURL = _mcp.options.apiBaseUrl + options.get;
+			var now = new Date();
 
+			if (_pageDataCache[options.page]){
+				if (_pageDataCache[options.page].expires >= now.getTime()){
+					//cache not yet expired, use existing data
+					//TODO: allow specifying prepend/append, maybe?
+					render( options.renders, options.container, _pageDataCache[options.page].data, true );
+					//TODO: allow setting `this` context of onComplete method, maybe?
+					if (options.onComplete) options.onComplete(options, _pageDataCache[options.page].data);
+					return;
+				}else{
+					delete _pageDataCache[options.page];
+				}
+			}
+
+			//TODO: assumes json
+			//TODO: allow setting custom timeout per page
+			//TODO: setting to allow+configure the jqm loading indicator
+			$.ajax({
+				url: requestURL
+				,type: 'GET'
+				,data: options.getData
+				,dataType: 'json'
+				,encoding: 'UTF-8'
+				,success: function(data, textStatus, jqXHR){
+					var cacheDuration = new Date().getTime();
+					cacheDuration += (options.cacheDuration * 1000); //add config value converted to ms
+					_pageDataCache[options.page] = {
+						expires: cacheDuration
+						, data: data
+					};
+					//TODO: allow specifying prepend/append, maybe?
+					render( options.renders, options.container, data, true );
+					//TODO: allow setting `this` context of onComplete method, maybe?
+					if (options.onComplete) options.onComplete(options, data);
+				}
+				,error: function(jqXHR, textStatus, errorThrown){
+					if (options.apiError) options.apiError(jqXHR, textStatus, errorThrown);
+				}
+				,complete: function(xhr){
+					console.log('MCP XHR Complete:', xhr);
+				}
+				,crossDomain: true
+				,cache: false
+			});
+		})
+		.on("pagehide", function(){
+			if (options.onTearDown) options.onTearDown(options.page);
 		});
 	};
 
